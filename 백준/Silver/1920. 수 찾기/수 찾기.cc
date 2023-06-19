a5c17c246a227da37169e6d88496874d578280dd
Luidingo <110881809+Luidingo@users.noreply.github.com>
2023-06-20 00:40:08 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,50 +1,53 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int N,M;
-int arr[100010];
+int n, m;
+int arr[100001];
 
-void binarySearch(int key){
-    int start = 0;
-    int end = N-1;
-    int mid;
-
-    while(end>=start){
-        mid =(start+end)/2;
-        if(arr[mid]==key){
-            cout<<1<<"\n";
-            return;
-        }else if(arr[mid]>key){
-            end = mid - 1;
-        }else{
-            start = mid + 1;
-        }
-    }
-    cout<<0<<"\n";
+void binarySearch(int val)
+{
+	int mid;
+	int fst = 0, scd = n - 1;
+	while (scd >= fst)
+	{
+		mid = (fst + scd) / 2;
+		if (val == arr[mid])
+		{
+			cout << 1 << "\n";
+			return;
+		}
+		else if (val < arr[mid])
+			scd = mid - 1;
+		else
+			fst = mid + 1;
+	}
+	cout << 0 << "\n";
     return;
 }
 
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
-int main(){
-    
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    cin>>N;
-    int temp;
+	cin >> n;
 
-    for(int i=0;i<N;i++){
-        cin>>temp;
-        arr[i]=temp;
-    }
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
 
-    sort(arr,arr+N);
+	sort(arr, arr + n);
 
-    cin>>M;
-    for(int i=0;i<M;i++){
-        cin>>temp;
-        binarySearch(temp);
-    }
+	cin >> m;
 
-    return 0;
+	int val;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> val;
+		binarySearch(val);
+	}
 }
