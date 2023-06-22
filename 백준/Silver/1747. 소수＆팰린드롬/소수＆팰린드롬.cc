7edeecdcb41c7100b696302834b368a2de50f58a
Luidingo <110881809+Luidingo@users.noreply.github.com>
2023-06-22 23:38:28 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cc
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cc
@@ -1,39 +1,30 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
-bool isPrime(int num)
-{
-    if (num < 2)
-        return false;
-	for (int i = 2; i < num; i++)
-	{
-		if (num % i == 0)
-			return false;
-	}
+bool isPrime(int N) {
+	if (N < 2) return false;
+	for (int i = 2; i < N; i++)
+		if (N % i == 0) return false;
 	return true;
 }
 
-bool isPalindrome(string s)
-{
-	string tmp = s;
-	reverse(s.begin(), s.end());
-	if (tmp == s)
-		return true;
+bool Pal(string N) {
+	string str = N;
+	reverse(N.begin(), N.end());
+	if (N == str) return true;
 	return false;
 }
 
-int main(void)
-{
-	int n;
+int main(void) {
+	int N;
+	cin >> N;
 
-	cin >> n;
-
-
-    for (int i = n;; i++)
-    {
-        if (isPalindrome(to_string(i)))
+	for (int i = N; ; i++) {
+		// 만약 i가 소수이고 팰린드롬이면
+		if (Pal(to_string(i)))
         {
             if (isPrime(i))
             {
@@ -41,5 +32,5 @@ int main(void)
                 return 0;
             }
         }
-    }
+	}
 }
